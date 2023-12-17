e25e5486c907ac8fa0057e779a114670d4997e0a
ht-truongdv <truongdv@hybrid-technologies.com.vn>
2023-12-17 17:38:07 +0700

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,12 @@ module.exports = {
   PASSWORD: "bf37917f43b6223f3eb3aa5f9be073111002e275ac82d9ca9dd27745275e0047",
   DB: "dbdh7imkv4p87t",
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
   pool: {
     max: 5,
     min: 0,
