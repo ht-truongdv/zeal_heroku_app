99749ed9034608f67c19682b45b2728deaefe864
ht-truongdv <truongdv@hybrid-technologies.com.vn>
2023-12-17 15:50:08 +0700

    Easy 4 Steps: Auto Deploy Vue.js App on Heroku https://www.codementor.io/@ravianand1988/easily-4-steps-to-continuous-delivery-auto-deploy-vue-js-app-on-heroku-xljk977pq

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+// const bodyParser = require("body-parser"); /* deprecated */
+// const cors = require("cors");
+
+const app = express();
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
+
+// app.use(cors(corsOptions));
+
+// // parse requests of content-type - application/json
+// app.use(express.json());  /* bodyParser.json() is deprecated */
+
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
+// const db = require("./app/models");
+// db.sequelize.sync();
+// // // drop the table if it already exists
+// // db.sequelize.sync({ force: true }).then(() => {
+// //   console.log("Drop and re-sync db.");
+// // });
+
+// // simple route
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to bezkoder application." });
+// });
+
+// require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
