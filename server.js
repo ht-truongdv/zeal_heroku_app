555e6cf8b33d7aef15835e67e15f477c667cab4d
ht-truongdv <truongdv@hybrid-technologies.com.vn>
2023-12-17 17:02:01 +0700

    example https://github.com/bezkoder/vue-3-crud/blob/master/vue.config.js https://www.bezkoder.com/vue-3-crud/

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-// const bodyParser = require("body-parser"); /* deprecated */
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
@@ -14,31 +13,31 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-// // parse requests of content-type - application/json
-// app.use(express.json());  /* bodyParser.json() is deprecated */
+// parse requests of content-type - application/json
+app.use(express.json());  /* bodyParser.json() is deprecated */
 
-// // parse requests of content-type - application/x-www-form-urlencoded
-// app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
 
-// const db = require("./app/models");
-// db.sequelize.sync();
-// // // drop the table if it already exists
-// // db.sequelize.sync({ force: true }).then(() => {
-// //   console.log("Drop and re-sync db.");
-// // });
-
-// // simple route
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to bezkoder application." });
+const db = require("./app/models");
+db.sequelize.sync();
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
 // });
 
-// require("./app/routes/turorial.routes")(app);
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+    port: 8081
+  }
+}
