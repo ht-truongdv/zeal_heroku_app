9163587cf17b1b3d3ac0f0ede8a8a791542ea17c
ht-namnn <namnn@hybrid-technologies.com.vn>
2023-12-20 18:46:01 +0700

    update template email slash n

--- app/batches/mail.alert.js
+++ app/batches/mail.alert.js
@@ -1,14 +1,16 @@
 const sendMail = require("../services/mailgun-sender");
-const mailTemp = require("../services/email-template.json");
+const mailTemp = require("../templates/email-template.json");
 
 const mailAlert = () =>{
-  let template = "templateA";
+  //gia lap data trong controller, return ve json
+  //loop record => lay email va add vao reciever[]
 
+  let template = "templateB";
   let emailConfig = {
-  sender: process.env.MAILGUN_SENDER,
-  receiver: [process.env.MAILGUN_RECIEVER],
-  subject: new Date() + mailTemp[template].Subject,
-  text: mailTemp[template].Body,
+    sender: "mailgun <truongdv@hybrid-technologies.com.vn>",
+    receiver: ["namnn@hybrid-technologies.com.vn"],
+    subject: new Date() + mailTemp[template].Subject,
+    html: mailTemp[template].Body,
   };
   sendMail(emailConfig);
 }
--- app/services/mailgun-sender.js
+++ app/services/mailgun-sender.js
@@ -1,17 +1,17 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_KEY});
+const mg = mailgun.client({username: 'api', key: "6eb83d4a6b328d348f46a331d680590d-5e3f36f5-907bf1e7"}); //process.env.KEY_MAILGUN
 
 sendMail = function (email_Config) {
     const data = {
         "from": email_Config.sender,
         "to": email_Config.receiver,
         "subject": email_Config.subject,
-		"html" : email_Config.text
+		"html" : email_Config.html
     };
  
-	mg.messages.create(process.env.MAILGUN_ORG, data)
+	mg.messages.create("sandboxbcbaed8e264c4099a494ec5fb10922c8.mailgun.org", data) //process.env.ORG_MAILGUN
 	.then(msg => console.log(msg)) 	// logs response data
 	.catch(err => console.log(err)); // logs any error
 }
